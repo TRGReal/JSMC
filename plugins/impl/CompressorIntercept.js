60f899dfe5736aca201930401abee9efc7287464
TRGReal <24951208+TRGReal@users.noreply.github.com>
2021-11-10 08:46:31 +0800

    Update CompressorIntercept.js

--- plugins/impl/CompressorIntercept.js
+++ plugins/impl/CompressorIntercept.js
@@ -4,12 +4,31 @@
 // https://nodejs.org/api/zlib.html
 // https://wiki.vg/Protocol#With_compression
 
+const zlib = require('zlib');
 const Plugin = require("../Plugin.js");
+const Packet = require("../../utils/packet/Packet.js");
 
 class CompressorIntercept extends Plugin {
 	
 	constructor() {
 		super("CompressorIntercept", "1.0", false);
+	}
+	
+	onPreWrite(packet) {
+		const PacketBuilder = this.getPacketBuilder();
+		const client = packet.getClient();
+		const data = packet.getData();
+		
+		if (client.compressionThreshold > 0) {
+			const NewPacket = new PacketBuilder();
+			
+			NewPacket.writeVarInt(data.length);
+			NewPacket.writeBytes(zlib.deflateSync(data, {
+				"level": zlib.constants.Z_BEST_COMPRESSION
+			});
+			
+			packet.setData(NewPacket.getResult());
+		}
 	}
 	
 }
